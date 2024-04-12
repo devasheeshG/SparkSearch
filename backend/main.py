3d5354c25f1017c88592714f0aa699873a06a03b
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-04-12 12:25:04 +0000

    pdf extraction method

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,10 @@
 
 import bcrypt, os
 from dotenv import load_dotenv
-from uuid import UUID
+from uuid import UUID, uuid4
 from typing import List
 from fastapi import FastAPI, Form, HTTPException, status, File, UploadFile
+from fastapi.responses import JSONResponse, Response
 from backend.database import Postgres, MongoDB, VectorDB
 from backend.ingestion import extract_chunks_from_document, embed_chunks
 from backend.utils.model import Chunk, Embedding, UploadedFile
@@ -15,41 +16,53 @@ app = FastAPI(
     title="SparkSearch Backend API",
     description="SparkSearch Backend API",
     version="0.0.1",
-    docs_url=None,
-    redoc_url=None,
+    docs_url='/api/docs',
+    redoc_url='/api/redocs',
 )
 
-postgress = Postgres(uri=os.getenv("POSTGRES_URI"), database=os.getenv("POSTGRES_DB"))
+postgress = Postgres(uri=os.getenv("POSTGRES_URI"), database=os.getenv("POSTGRES_DATABASE"))
+postgress.connect()
 mongodb = MongoDB(uri=os.getenv("MONGODB_URI"), database=os.getenv("MONGODB_DATABASE"))
+mongodb.connect()
 vectordb = VectorDB(uri=os.getenv("MILVUS_URI"), database_name=os.getenv("MILVUS_DATABASE"))
+vectordb.connect()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the SparkSearch API."}
 
 @app.post("/api/auth/register")
 async def register(username: str = Form(...), password: str = Form(...)):
     """Register a new user."""
-    # Check if the user already exists in the database
-    postgress.execute(f"SELECT * FROM users WHERE username='{username}'")
-    if postgress.cursor.fetchone():
-        raise HTTPException(status_code=400, detail="User already exists, please login.")
+    try:
+        # Check if the user already exists in the database
+        postgress.cursor.execute(f"SELECT * FROM users WHERE username='{username}'")
+        if postgress.cursor.fetchone():
+            raise HTTPException(status_code=400, detail="User already exists, please login.")
 
-    # Hash the password with a salt
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        # Hash the password with a salt
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-    # Insert the user into the database
-    postgress.execute(f"INSERT INTO users (username, password_hash, salt) VALUES ('{username}', '{hashed_password.decode('utf-8')}', '{salt.decode('utf-8')}')")
+        # Insert the user into the database
+        postgress.cursor.execute(f"INSERT INTO users (username, password_hash, salt) VALUES ('{username}', '{hashed_password.decode('utf-8')}', '{salt.decode('utf-8')}')")
 
-    return {"status": "success", "message": "User registered successfully."}, status.HTTP_201_CREATED
+        return JSONResponse(content={"status": "success", "message": "User registered successfully."}, status_code=status.HTTP_201_CREATED)
+    
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/api/auth/login")
-async def verify(username: str = Form(...), password: str = Form(...)):
+async def login(username: str = Form(...), password: str = Form(...)):
     """Verify a user."""
-    postgress.execute(f"SELECT id, password_hash, salt FROM users WHERE username='{username}'")
+    postgress.cursor.execute(f"SELECT id, password_hash, salt FROM users WHERE username='{username}'")
     result = postgress.cursor.fetchone()
     if result:
         user_id, hashed_password, salt = result
         rehashed_password = bcrypt.hashpw(password.encode('utf-8'), salt.encode('utf-8'))
         if rehashed_password.decode('utf-8') == hashed_password:
-            return {"status": "success", "message": "User verified successfully.", "id": user_id}, status.HTTP_200_OK
+            return JSONResponse(content={"status": "success", "message": "User verified successfully.", "user_id": user_id}, status_code=status.HTTP_200_OK)
 
     raise HTTPException(status_code=400, detail="Invalid username or password.")
 
@@ -58,52 +71,66 @@ async def upload_document(file: UploadFile = File(...), path: str = Form(...), u
     """
     Upload a single PDF, PowerPoint, or Word document and embed it into the vector database.
     """
-    file_type = file.filename.split(".")[-1].lower()
-    if file_type in ["pdf"]:
-        # Insert the file into the database
-        if file_type == "pdf":
-            import PyPDF2
-            pdf = PyPDF2.PdfFileReader(file.file)
-            page_nums = pdf.getNumPages()
-        
-        file_data = UploadedFile(
-            id=None,
-            user_id=user_id,
-            file_path=path,
-            file_name=file.filename,
-            file_type=file_type,
-            page_count=page_nums
-        )
-        
-        postgress.execute(f"INSERT INTO files (user_id, file_path, file_name, file_type, page_count) VALUES ('{file_data.user_id}', '{file_data.file_path}', '{file_data.file_name}', '{file_data.file_type}', '{file_data.page_count}') RETURNING id")
-        file_data.id = postgress.cursor.fetchone()[0]
- 
-        chunks: List[Chunk] = await extract_chunks_from_document(file, file_data.id)
-        embedding_data: List[Embedding] = await embed_chunks(chunks)
-        
-        for data in embedding_data:
-            # Insert the chunk text into the MongoDB database
-            inserted_id = mongodb.client.insert_one(
-                collection_name=mongodb.collection_name,
-                document=data.chunk.model_dump()
-            ).inserted_id
+    try:
+        # Check if `user_id` exists in the database
+        postgress.cursor.execute(f"SELECT * FROM users WHERE id='{user_id}'")
+        if not postgress.cursor.fetchone():
+            raise HTTPException(status_code=400, detail="User does not exist.")
+
+        file_type = file.filename.split(".")[-1].lower()
+        if file_type in ["pdf", "rtf"]:
+            # Insert the file into the database
+            if file_type == "pdf":
+                import PyPDF2
+                reader = PyPDF2.PdfReader(file.file)
+                page_count = len(reader.pages)
             
-            # Assign the inserted ID to data.id after converting it to a string
-            data.id = str(inserted_id)
-            
-            # Insert the embeddings into the Milvus database
-            vectordb.client.insert(
-                collection_name=vectordb.collection_name,
-                data={
-                    "chunk_id": str(data.chunk._id),
-                    "file_id": str(data.chunk.file_id),
-                    "embeddings": data.embeddings
-                }
+            if file_type == "rtf":
+                page_count = 0
+        
+            file_data = UploadedFile(
+                user_id=user_id,
+                file_path=path,
+                file_name=file.filename,
+                file_type=file_type,
+                page_count=page_count
             )
             
-        return {"status": "success", "message": "Document uploaded and embedded successfully.", "file_id": file_data.id}, status.HTTP_201_CREATED
-    else:
-        raise ValueError(f"Unsupported file type: {file_type}")
+            postgress.cursor.execute(f"INSERT INTO files (user_id, file_path, file_name, file_type, page_count) VALUES ('{file_data.user_id}', '{file_data.file_path}', '{file_data.file_name}', '{file_data.file_type}', '{file_data.page_count}') RETURNING id")
+            file_data.id = postgress.cursor.fetchone()[0]
+    
+            chunks: List[Chunk] = await extract_chunks_from_document(file, file_data)
+        #     embedding_data: List[Embedding] = await embed_chunks(chunks)
+            
+        #     for data in embedding_data:
+        #         # Insert the chunk text into the MongoDB database
+        #         inserted_id = mongodb.client.insert_one(
+        #             collection_name=mongodb.collection_name,
+        #             document=data.chunk.model_dump()
+        #         ).inserted_id
+                
+        #         # Assign the inserted ID to data.id after converting it to a string
+        #         data.id = str(inserted_id)
+                
+        #         # Insert the embeddings into the Milvus database
+        #         vectordb.client.insert(
+        #             collection_name=vectordb.collection_name,
+        #             data={
+        #                 "chunk_id": str(data.chunk._id),
+        #                 "file_id": str(data.chunk.file_id),
+        #                 "embeddings": data.embeddings
+        #             }
+        #         )
+
+            return JSONResponse(content={"status": "success", "message": "Document uploaded and embedded successfully.", "file_id": str(file_data.id)}, status_code=status.HTTP_201_CREATED)
+        
+        else:
+            raise HTTPException(status_code=400, detail="Invalid file type. Please upload a PDF, PowerPoint, or Word document.")
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/api/delete_file")
 async def delete_file(file_id: str = Form(...)):
@@ -111,19 +138,19 @@ async def delete_file(file_id: str = Form(...)):
     Delete the document from the database.
     """
     try:
-        postgress.execute(f"DELETE FROM files WHERE id='{file_id}'")
+        postgress.cursor.execute(f"DELETE FROM files WHERE id='{file_id}'")
         mongodb.client.delete_many(collection_name=mongodb.collection_name, query={"file_id": file_id})
         vectordb.client.delete(collection_name=vectordb.collection_name, query={"file_id": file_id})
-        return {"status": "success", "message": "Document deleted successfully."}, status.HTTP_200_OK
+        return JSONResponse(content={"status": "success", "message": "Document deleted successfully."}, status_code=status.HTTP_200_OK)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/api/get_file_ids")
 async def get_file_ids(user_id: str = Form(...)):
     """Get all the file IDs for the given user."""
-    postgress.execute(f"SELECT id, file_path, file_name, file_type, page_count FROM files WHERE user_id='{user_id}'")
+    postgress.cursor.execute(f"SELECT id, file_path, file_name, file_type, page_count FROM files WHERE user_id='{user_id}'")
     file_ids = postgress.cursor.fetchall()
-    return {"status": "success", "message": "File IDs retrieved successfully.", "file_ids": file_ids}, status.HTTP_200_OK
+    return JSONResponse(content={"status": "success", "message": "File IDs retrieved successfully.", "file_ids": file_ids}, status_code=status.HTTP_200_OK)
 
 @app.post("/api/search")
 async def search(query: str = Form(...), included_file_ids: List[str] = Form(...)):
